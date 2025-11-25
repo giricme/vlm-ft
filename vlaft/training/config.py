ada9d1c3647f058fc756ac72bb15e84108f76eda
Giri Anantharaman <giria@andrew.cmu.edu>
2025-11-25 14:21:04 -0500

    Fixing

--- vlaft/training/config.py
+++ vlaft/training/config.py
@@ -19,6 +19,7 @@ class DataConfig:
     stage: int = 1
     max_frames: int = 16
     max_length: int = 2048
+    max_dynamic_patch: int = 4 
     subset_ratio: float = 1.0
     max_samples: Optional[int] = None
     num_workers: int = 4
